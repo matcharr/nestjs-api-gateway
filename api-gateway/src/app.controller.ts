204562508b52f5fd17540978cfe5171f96707d01
Dev <dev@staff.local>
2021-04-26 11:52:14 +0000

    done

---
+++ api-gateway/src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get } from '@nestjs/common';
+import { zip } from 'rxjs';
+import { AppService } from './app.service';
+import { map } from 'rxjs/operators';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/ping-a')
+  pingServiceA() {
+    return this.appService.pingServiceA();
+  }
+
+  @Get('/ping-b')
+  pingServiceB() {
+    return this.appService.pingServiceB();
+  }
+
+  @Get('/ping-all')
+  pingAll() {
+    return zip(
+      this.appService.pingServiceA(),
+      this.appService.pingServiceB(),
+    ).pipe(
+      map(([pongServiceA, pongServiceB]) => ({
+        pongServiceA,
+        pongServiceB,
+      })),
+    );
+  }
+}
